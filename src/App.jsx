ab321453695600b73d1e6606236a3096ac8423fd
mahinur05 <mahi121.mr2@gmail.com>
2023-03-28 14:55:43 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import React from 'react'
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+
+const App = () => {
+  return (
+    <div>App</div>
+  )
+}
+
+export default App
