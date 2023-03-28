b6b9da158f1644dc27796f239e115c8f171d3806
mahinur05 <mahi121.mr2@gmail.com>
2023-03-28 15:04:52 +0600

    components added

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,26 @@ import {
   Routes,
   Route,
 } from "react-router-dom";
+import { Box } from '@mui/system';
+import Navbar from './components/Navbar';
+import Feed from './components/Feed';
+import VideoDetails from './components/VideoDetails';
+import ChannelDetail from './components/ChannelDetail';
+import SearchFeed from './components/SearchFeed';
 
 const App = () => {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <Box sx={{backgroundColor: '#000'}}>
+        <Navbar />
+        <Routes>
+          <Route path="/" exact element={<Feed />} />
+          <Route path="/video/:id" element={<VideoDetails />} />
+          <Route path="/channel/:id" element={<ChannelDetail />} />
+          <Route path="/search/:searchTerm:" element={<SearchFeed />} />
+        </Routes>
+      </Box>
+    </BrowserRouter>
   )
 }
 
