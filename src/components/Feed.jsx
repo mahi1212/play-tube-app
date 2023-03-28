dfdb44671f76f9f644b800a16309934e409e0e99
mahinur05 <mahi121.mr2@gmail.com>
2023-03-28 16:23:19 +0600

    feed video component addded

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,21 +1,29 @@
 import { Typography } from '@mui/material'
 import { Box, Stack } from '@mui/system'
 import React from 'react'
+import Sidebar from './Sidebar'
+import Videos from './Videos'
 
 const Feed = () => {
   return (
     <Stack sx={{
-      flexDirection: { xs: 'column', md: 'row' }
+      flexDirection: { xs: 'column', md: 'row',}
     }}>
       <Box sx={{
-        height: { xs: 'auto', md: '92vh' },
+        height: { xs: 'auto', md: '93vh' },
         borderRight: '1px solid #3d3d3d',
         px: {sx: 0, md: 2}
       }}>
-        Sidebar
+        <Sidebar />
         <Typography variant='body2' className='copyright' sx={{mt:2, color: '#fff'}}>
           &copy; Copyright 2023
         </Typography>
+      </Box>
+      <Box p={2} sx={{overflowY: 'auto', height: '90vh', flex: 1}}>
+        <Typography sx={{ml: 2, color: 'white'}} variant='h4'>
+          New <span style={{textTransform: 'uppercase', color: '#F31503'}}>videos</span>
+        </Typography>
+        <Videos videos={[]}/>
       </Box>
     </Stack>
   )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Stack } from '@mui/system';
 import { Link } from 'react-router-dom';
-import { logo } from '../utils/constant';
+import { logo } from '../utils/constants';
 import SearchBar from './SearchBar';
 const Navbar = () => {
   return (
